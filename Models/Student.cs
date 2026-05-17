85b2da0df663b86be5fcf9e6184b2f44a2b23168
Doha-bassam <dbassam25@gmail.com>
2026-05-17 16:36:51 +0300

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication3.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Major { get; set; }
+        public double GPA { get; set; }
+    }
+}
